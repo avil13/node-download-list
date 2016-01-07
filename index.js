d828e8bd31bf99a534572e3eb85d5fbad58d58d2
avil13 <13.avil@gmail.com>
2016-01-08 01:36:10 +0300

    :scissors:

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var download_list = require('./list'); // список файлов
 
 // Название папки и список
 var list = download_list.list;
-var folder = 'download/' + download_list.list_name + '/';
+var folder = 'download/' + download_list.name + '/';
 mkdirp(folder); // Создание папки
 
 // Прогресс бар
@@ -21,18 +21,14 @@ var bar = new ProgressBar('  Downloading [:bar] :percent :etas', {
 
 // Имя файла
 var list_copy = list.slice();
-var nameL = function(name) {
-    var num;
-    for (num = list_copy.length; num--;) {
-        if (list_copy[num].title === name) {
-            break;
-        }
-    }
-    ++num;
+var nameL = function(el) {
+    var num = list_copy.indexOf(el) + 1;
+    var ext = el.url.split('.').pop(); // расширение файла
+
     if (num < 10) {
         num = '0' + num;
     }
-    return num + '. ' + name;
+    return num + '. ' + (el.title || '') + '.' + ext;
 };
 
 
@@ -43,11 +39,10 @@ var nameL = function(name) {
     if (list_arr.length === 0) {
         return false;
     }
-    var el = list_arr.splice(0, 1),
-        name = el[0].title,
-        path = el[0].url;
+    var el = list_arr.splice(0, 1)[0],
+        path = el.url;
 
-    var file = fs.createWriteStream(folder + nameL(name) + '.mp4');
+    var file = fs.createWriteStream(folder + nameL(el));
     var request;
 
     // Загрузка
